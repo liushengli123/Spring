4b89612f4cdfc0c33637808f04905e84b17b4837
victory <victory@liu.com>
2020-12-01 11:36:31 +0800

    this is spring AOP second

--- Spring-AOP/src/main/java/com/SpringAOP/MyAspectj.java
+++ Spring-AOP/src/main/java/com/SpringAOP/MyAspectj.java
@@ -1,7 +1,36 @@
 package com.SpringAOP;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.stereotype.Component;
+
+@Component("myAspectj")
+@Aspect
 public class MyAspectj {
+    @Before("pointcut()")
     public void before(){
-        System.out.println("this is 前置增强");
+        System.out.println("this is 前置通知");
     }
+    @AfterReturning("pointcut()")
+    public void afterReturn(){
+        System.out.println("this is 后置通知");
+    }
+    @Around("execution(* com.SpringAOP.Target.*(..)))")
+    public Object  around(ProceedingJoinPoint pjb) throws Throwable {
+        System.out.println("this is 环绕通知");
+        Object proceed = pjb.proceed();
+        System.out.println("this is 环绕通知");
+        return proceed;
+    }
+    @After("execution(* com.SpringAOP.Target.*(..)))")
+    public void after(){
+        System.out.println("this is 最终通知");
+    }
+    @AfterThrowing("execution(* com.SpringAOP.Target.*(..)))")
+    public void afterThrowing(){
+        System.out.println("this is 异常通知");
+    }
+    @Pointcut("execution(* com.SpringAOP.Target.*(..)))")
+    public void pointcut(){};
 }
--- Spring-AOP/src/main/java/com/SpringAOP/Target.java
+++ Spring-AOP/src/main/java/com/SpringAOP/Target.java
@@ -3,8 +3,10 @@ package com.SpringAOP;
 import com.jdkAOP.TargitInterface;
 import org.springframework.stereotype.Component;
 
+@Component("target")
 public class Target implements TargetInterface{
     public void save() {
         System.out.println("这是一个连接点");
+//        int a=1/0;
     }
 }
